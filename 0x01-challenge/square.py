f57f708eac39c2b215065e4136d4098a3a32b148
dannycod3r <oseid7807@gmail.com>
2024-01-22 01:27:42 +0000

    Capitalize the class name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -3,7 +3,7 @@
 """
 
 
-class square():
+class Square():
     """Square class
 
     Methods:
@@ -34,7 +34,7 @@ class square():
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
